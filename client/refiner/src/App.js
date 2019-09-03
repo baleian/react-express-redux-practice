340430e6bab162548f5e511682cf4915343cb527
baleian <baleian90@gmail.com>
2019-09-03 22:30:09 +0900

    Added navigator and containers

--- client/refiner/src/App.js
+++ client/refiner/src/App.js
@@ -1,11 +1,21 @@
 import React from 'react';
-
 import './App.css';
+
+import Navigator from './components/Navigator';
+import { BrowserRouter, Route } from 'react-router-dom';
+import GpsRefinerContainer from './containers/GpsRefinerContainer';
+import StoreRefinerContainer from './containers/StoreRefinerContainer';
+import AddressRefinerContainer from './containers/AddressRefinerContainer';
 
 function App() {
   return (
     <div className="App">
-      <h1>React App!</h1>
+      <BrowserRouter>
+        <Navigator />
+        <Route path={process.env.PUBLIC_URL + "/Gps"} component={GpsRefinerContainer} />
+        <Route path={process.env.PUBLIC_URL + "/Store"} component={StoreRefinerContainer} />
+        <Route path={process.env.PUBLIC_URL + "/Address"} component={AddressRefinerContainer} />
+      </BrowserRouter>
     </div>
   );
 }
---
+++ client/refiner/src/containers/GpsRefinerContainer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const GpsRefinerContainer = () => {
+    return (
+        <div>
+            <h1>GPS Refiner</h1>            
+        </div>
+    );
+};
+
+export default GpsRefinerContainer;
